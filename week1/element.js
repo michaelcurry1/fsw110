a424a5236b8b5617700eeb8de4a96baa90f5c81f
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-02-26 11:22:28 -0500

    week1 assignment

--- week1/element.js
+++ week1/element.js
@@ -1,2 +1,4 @@
 var motorcycles = document.getElementById('Harley Davidson');
 console.log(motorcycles)
+var motorcycles = document.getElementById('harleyDavidson');
+motorcycles.innerHTML="Harley Davidson";
