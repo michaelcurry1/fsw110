38facc2ea501d6f0d66851164a5a42fc668e4a4f
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-03-05 16:18:09 -0500

    resubmitWeek2

--- week2/project1.js
+++ week2/project1.js
@@ -1,8 +1,34 @@
 var nav1 = document.createElement("nav")
 document.body.appendChild(nav1)
+var a1 = document.createElement("a")
+a1.textContent = "https://www.mockplus.com/blog/post/bootstrap-navbar-template"
+var a2 = document.createElement("a")
+a2.textContent = "https://colorlib.com/wp/bootstrap-navbar/"
+var a3 = document.createElement("a")
+a3.textContent = "https://designmodo.com/bootstrap-navbar/"
 
 var h1 = document.createElement("h1")
 document.body.appendChild(h1)
+h1.textContent = 'Welcome to my JS 1st site'
 
+var p1 = document.createElement("p")
+document.body.appendChild(p1)
+p1.textContent = 'This is my paragraph'
 
+var newOl = document.createElement("ol")
+document.body.appendChild(newOl)
+var li = document.createElement("li")
+li.textContent = "cow"
+newOl.appendChild(li)
 
+var li1 = document.createElement("li")
+li1.textContent = "cat"
+newOl.appendChild(li1)
+
+var li2 = document.createElement("li")
+li2.textContent = "dog"
+newOl.appendChild(li2)
+
+var footer1 = document.createElement("footer")
+document.body.appendChild(footer1)
+footer1.textContent = 'This is the footer'
