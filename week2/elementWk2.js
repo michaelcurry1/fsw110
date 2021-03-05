7c69a780ce7faed83d25974ddd9b36fe1d82bc60
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-03-05 13:41:04 -0500

    110week2resubmit

--- week2/elementWk2.js
+++ week2/elementWk2.js
@@ -3,10 +3,11 @@ newE.textContent = 'Welcome to my JS site'
 document.body.appendChild(newE)
 
 var newP = document.createElement('p')
-newP.textContent = 'All of this was created with JavaScrip'
+newP.textContent = 'All of this was created with JavaScript'
 document.body.appendChild(newP)
 
-var newL = document.createElement(li)
-newL.textContent = 'cow'
-var myList = document.getElementById('my-list')
-myList.appendChild(newL)
+var newOl = document.createElement("ol")
+document.body.appendChild(newOl)
+var li = document.createElement("li")
+li.textContent = "cow"
+newOl.appendChild(li)
