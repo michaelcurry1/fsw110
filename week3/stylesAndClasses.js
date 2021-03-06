66d1afc0b49a1fe32113485fef451d8f45cbd0b8
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-03-06 13:57:32 -0500

    week3StylesAndClasse

---
+++ week3/stylesAndClasses.js
@@ -0,0 +1,9 @@
+var motorcycles = ['harley','suzuki','honda','yamaha','indian']
+var motorcycleList = document.getElementById('motorcycle')
+
+for(var i = 0; i < motorcycles.length; i++){
+
+}
+var newName = documen.createElement('h2')
+newMame.textContent = motorcycles[0]
+motorcycleList.append(newName)
