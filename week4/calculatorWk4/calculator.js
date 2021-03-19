bf2c7bcb3953a2f040a7383c1c53d64ce4cb5ef5
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-03-19 10:26:54 -0400

    calculatorDone

--- week4/calculatorWk4/calculator.js
+++ week4/calculatorWk4/calculator.js
@@ -13,32 +13,33 @@ ans.textContent = x
 
 });
 
-function add(a, b){
-    var w = a + b;
+function sub(a, b){
+    var w = a - b;
     return w
 }
 
-var addButton = document.getElementById("addButton")
-addButton.addEventListener("click", function(){
-var firstAdd = Number(document.getElementById("firstAddN").value);
-var secondAdd = Number(document.getElementById("secondAddN").value);
-var x = add(firstAdd,secondAdd)
+var subButton = document.getElementById("subButton")
+subButton.addEventListener("click", function(){
+var firstsub = Number(document.getElementById("firstSubN").value);
+var secondsub = Number(document.getElementById("secondSubN").value);
+var x = sub(firstsub,secondsub)
 var ans = document.getElementById("answer")
 ans.textContent = x
 
 });
 
-function add(a, b){
-    var w = a + b;
+function mul(a, b){
+    var w = a * b;
     return w
 }
 
-var addButton = document.getElementById("addButton")
-addButton.addEventListener("click", function(){
-var firstAdd = Number(document.getElementById("firstAddN").value);
-var secondAdd = Number(document.getElementById("secondAddN").value);
-var x = add(firstAdd,secondAdd)
+var mulButton = document.getElementById("mulButton")
+mulButton.addEventListener("click", function(){
+var firstMul = Number(document.getElementById("firstMulN").value);
+var secondMul = Number(document.getElementById("secondMulN").value);
+var x = mul(firstMul,secondMul)
 var ans = document.getElementById("answer")
 ans.textContent = x
 
 });
+
